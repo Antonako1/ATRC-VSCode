eb432d95e40395269f3e7af6d626e68edfeaa89a
Antonako1 <114558368+Antonako1@users.noreply.github.com>
2025-07-31 15:10:01 +0300

    working extension

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+//@ts-check
+'use strict';
+
+const path = require('path');
+
+/**@type {import('webpack').Configuration[]} */
+const config = [
+  {
+    name: 'extension',
+    target: 'node',
+    entry: './src/extension.ts',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'extension.js',
+      libraryTarget: 'commonjs2',
+      devtoolModuleFilenameTemplate: '../[resource-path]'
+    },
+    devtool: 'source-map',
+    externals: {
+      vscode: 'commonjs vscode'
+    },
+    resolve: {
+      mainFields: ['browser', 'module', 'main'],
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: 'ts-loader'
+        }
+      ]
+    }
+  },
+
+  {
+    name: 'server',
+    target: 'node',
+    entry: './src/server.ts',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'server.js',
+      libraryTarget: 'commonjs2',
+      devtoolModuleFilenameTemplate: '../[resource-path]'
+    },
+    devtool: 'source-map',
+    externals: {
+      vscode: 'commonjs vscode'
+    },
+    resolve: {
+      mainFields: ['browser', 'module', 'main'],
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: 'ts-loader'
+        }
+      ]
+    }
+  }
+];
+
+module.exports = config;
